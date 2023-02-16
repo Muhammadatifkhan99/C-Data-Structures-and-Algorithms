ec1acfba9ba3fc573bc6e8bf99e50afe5ce11a14
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2023-02-16 17:32:58 +0500

    Introducing Arrays in cpp

---
+++ Arrays/ArraysInroduction.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+	
+//	int a [4] = {1,2};
+//	cout<<a[0]<<" "<<a[1]<<" "<<a[2]<<" "<<a[3]<<endl;
+	
+//	char a[10]= {"Pakistan"};
+//	for (int i= 0;i < 10;i++){
+//		cout<<a[i]<<endl;
+//	}
+//	cout<<a[0]<<a[1]<<endl;
+
+	int a[10] = {1,2,3,4};
+	cout<<a[0]<<a[1]<<a[2]<<a[3]<<a[4]<<a[5]<<a[6]<<a[7]<<a[8]<<a[9]<<endl;	
+	
+	
+	
+	
+	
+	return 0;
+}
