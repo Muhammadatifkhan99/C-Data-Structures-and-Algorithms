94a89e59356408b4158c8fb6ad67bae5ae4a66db
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2023-02-16 17:33:20 +0500

    More practice on arrays

---
+++ ArraysIntroduction/main.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    
+    //	int a [4] = {1,2};
+//	cout<<a[0]<<" "<<a[1]<<" "<<a[2]<<" "<<a[3]<<endl;
+	
+//	char a[10]= {"Pakistan"};
+//	for (int i= 0;i < 10;i++){
+//		cout<<a[i]<<endl;
+//	}
+//	cout<<a[0]<<a[1]<<endl;
+
+	int a[10] = {1,2,3,4};	
+    for (int i = 0;i < 10;i ++){
+        cout<<a[i]<<endl;
+    }
+    //	cout<<a[0]<<a[1]<<a[2]<<a[3]<<a[4]<<a[5]<<a[6]<<a[7]<<a[8]<<a[9]<<endl;
+    
+//    int a[10] {1};
+//    for (int i = 0;i < 10;i++){
+//        cout<<a[i]<<endl;
+//    }
+//         int a[10]; fill_n(a, 10, -1);
+//    for (int i = 0;i < 10;i++){
+//        cout<<a[i]<<endl;
+//    }
+    //fill_n(array name, 100, -1); this segment will set all the elements of the array to a single value.
+    
+//    int a[10];
+//    for (int i = 0;i < 10; i ++){
+//        cout<<a[i]<<endl;//this will set all values to  a garbage values
+//    }
+//        int a [];error
+        
+        
+        
+        //Size of operator on array
+//        int a [10];
+//        cout<<sizeof(a)<<endl;
+//        int a1;
+//        cout<<sizeof(a1)<<endl;
+//        char name;
+//        cout<<sizeof(name)<<endl;
+//        string name_val;
+//        cout<<sizeof(name_val)<<endl;
+//        float gpa;
+//        cout<<sizeof(gpa)<<endl;
+//        double cgpa;
+//        cout<<sizeof(cgpa)<<endl;
+//        unsigned long long int aaa;
+//        cout<<sizeof(aaa)<<endl;
+
+
+
+
+    
+    return 0;
+}
